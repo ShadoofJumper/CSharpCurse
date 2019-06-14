0cc818981926da72f9401ad8f0a86956f7a56f93
Ihor <ShadoofJumper@gmail.com>
2019-06-14 23:26:51 +0300

    Create scrit for bot movement
    Create a simple script  dor movement a cube. Now its like a game for catch a bo in field)

---
+++ Assets/Scripts/WanderingAI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour
+{
+    public float speed = 1.5f; //значение скорости
+    public float obstacleRange = 0.5f; // рейнж проверки
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime); // движемся вперед каждій кадр
+
+        Ray ray = new Ray(transform.position, transform.forward); // создаем луч из центра обьекта в направлении обєкта
+        RaycastHit hit; 
+
+        if (Physics.SphereCast(ray, 0.75f, out hit))
+        {
+            if (hit.distance < obstacleRange) {
+
+                float angle = Random.Range(-110, 100);
+                transform.Rotate(0, angle, 0);
+
+            }
+        }
+    }
+}
