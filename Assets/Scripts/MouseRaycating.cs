bf84793df9bc4c82dbaba5ebdd48c729053aa205
Ihor <ShadoofJumper@gmail.com>
2019-07-19 22:11:37 +0300

    Create a UI
    Create a ui canvas, add button, ing and text. Elso add a logic for clic burron.

--- Assets/Scripts/MouseRaycating.cs
+++ Assets/Scripts/MouseRaycating.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MouseRaycating : MonoBehaviour
 {
@@ -10,15 +11,15 @@ public class MouseRaycating : MonoBehaviour
     {
         _camera = GetComponent<Camera>();
         //Hide cursor
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+       // Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) &&  !(EventSystem.current.IsPointerOverGameObject()))
         {
             Vector3 point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
             Ray ray = _camera.ScreenPointToRay(point);
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Text scoreLabel;
+
+    void Update()
+    {
+        scoreLabel.text = Time.realtimeSinceStartup.ToString();
+    }
+
+    public void OnOpenSettings()
+    {
+        Debug.Log("open settings");
+    }
+
+    public void OnPointerDown()
+    {
+        Debug.Log("pointer down");
+    }
+}
